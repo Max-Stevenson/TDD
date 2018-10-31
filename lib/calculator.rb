6fefb10b94e791ed11d56dd62d43a49c448b93e3
Max Stevenson <maxstevenson@msn.com>
2018-10-31 11:27:00 +0000

    Developed rspec test for a square() method of calculator class
    Wrote a square() method for calculator clas that utilised multiply() method to return expected output
    
    Both feature and unit tests pass as expected

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -2,4 +2,8 @@ class Calculator
 	def multiply (num1, num2)
 		num1 * num2
 	end
+	def square (num1)
+		multiply(num1, num1)
+	end
+
 end
--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -4,4 +4,7 @@ describe Calculator do
 	describe '#multiply' do
 		it { should respond_to(:multiply) }
 	end
+	describe '#square' do
+		it { should respond_to(:square) }
+	end
 end
