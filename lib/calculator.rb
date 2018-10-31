4ff918e1ef8eae2b7dc328eb782bc2faa9ad42be
Max Stevenson <maxstevenson@msn.com>
2018-10-31 12:40:12 +0000

    Developed a unit test for output_answer() method
    Wrote code for output_answer() method that passes both feature and unit tests

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -2,8 +2,12 @@ class Calculator
 	def multiply (num1, num2)
 		num1 * num2
 	end
+	
 	def square (num1)
 		multiply(num1, num1)
 	end
 
+	def output_answer
+		puts "the answer is xxxxxx"
+	end
 end
--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -4,7 +4,12 @@ describe Calculator do
 	describe '#multiply' do
 		it { should respond_to(:multiply) }
 	end
+
 	describe '#square' do
 		it { should respond_to(:square) }
+	end
+
+	describe '#output_answer' do
+		specify { expect { print("the answer is xxxxxx") }.to output.to_stdout }
 	end
 end
